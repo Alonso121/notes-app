640b0d9714d5f98de49f10f720d486ece5b0396f
alison <alison.tahiri@gmail.com>
2021-11-09 13:27:16 +0100

    remove red bg and sort tasks

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,9 +13,11 @@ import { getNotes } from "../redux/notes";
 function Home() {
   const dispatch = useDispatch();
   const accessToken = localStorage.accessToken;
-  const notes = useSelector((state) => state.notes);
+  const stateNotes = useSelector((state) => state.notes);
   const [toggleNewNote, setToggleNewNote] = useState(false);
   const [toggleUserModal, setToggleUserModal] = useState(false);
+
+  const notes = [...stateNotes]
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -30,7 +32,7 @@ function Home() {
 
   return (
     <Layout>
-      <div className="max-w-4xl w-full pb-4 relative bg-red-500">
+      <div className="max-w-4xl w-full pb-4 relative bg-gray-200 bg-opacity-20">
         <Menu
           setToggleUserModal={setToggleUserModal}
           toggleUserModal={toggleUserModal}
@@ -47,14 +49,14 @@ function Home() {
         )}
         {/* Notes list */}
         <div className="relative z-10 mx-auto px-4 mt-4">
-          {notes.map((note) => (
+          {notes.sort((a,b) => a.complete - b.complete).map((note) => (
             <Note key={note._id} note={note} token={accessToken} />
           ))}
         </div>
         {/* Add new note button */}
         <button
           onClick={() => setToggleNewNote(!toggleNewNote)}
-          className="fixed border border-gray-300 md:absolute z-40 bottom-12 right-14  flex items-center justify-center w-12 h-12 ml-2 text-2xl text-white bg-purple-600 rounded-full hover:bg-purple-700"
+          className="fixed shadow-md md:absolute z-40 bottom-12 right-14  flex items-center justify-center w-12 h-12 ml-2 text-2xl text-white bg-purple-600 rounded-full hover:bg-purple-700"
           aria-label="Add new note"
         >
           <IoAdd />
